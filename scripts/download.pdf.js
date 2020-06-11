57cd0df2b4e939db71b589a8d98e1fa87c67c073
McToluene <xclusiveniggaz10@live.com>
2020-06-12 00:44:37 +0100

    Key generator added

--- scripts/download.pdf.js
+++ scripts/download.pdf.js
@@ -1,7 +1,15 @@
-let fn = (function(){
-    return {
-        main: function (){
-            
-        }
-    }
+let fn = (function () {
+  return {
+    main: function () {},
+    generateKey: function () {
+      let key = "";
+      const characters =
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+      const charactersLength = 6;
+      for (var i = 0; i < charactersLength; i++) {
+        key += characters.charAt(Math.floor(Math.random() * charactersLength));
+      }
+      return key;
+    },
+  };
 })();
