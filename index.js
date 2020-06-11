67314d6c0df67d87647f90690a4e203ab37e1a7d
dudox <trybemark@gmail.com>
2020-06-11 22:07:29 +0100

    updates

---
+++ index.js
@@ -0,0 +1,32 @@
+// Require express and body-parser
+const express = require("express")
+const bodyParser = require("body-parser")
+const secret = "@dm1n..12345";
+const repo = "/var/www/book/";
+
+const http = require('http');
+const crypto = require('crypto');
+const exec = require('child_process').exec;
+
+
+// Initialize express and define a port
+const app = express()
+const PORT = 3000
+
+// Tell express to use body-parser's JSON parsing
+
+app.use(bodyParser.json())
+
+app.post("/deploy", (req, res) => {
+    
+    let sig = "sha1=" + crypto.createHmac('sha1', secret).update(chunk.toString()).digest('hex');
+
+    if (req.headers['x-hub-signature'] == sig) {
+        exec('cd ' + repo + ' && git pull');
+    }
+
+})
+
+
+// Start express on the defined port
+app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`))
