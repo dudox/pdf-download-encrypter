812627d24f6809f67fad5b77f86794ac310caa28
dudox <trybemark@gmail.com>
2020-06-12 12:20:08 +0100

    updates

--- encrypt.sh
+++ encrypt.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 pdftk main.pdf output "$1".pdf owner_pw 12345 user_pw "$1"
 echo "your password is $1" > readme.txt
-zip -r -Z bzip2 "$2".zip "$1".pdf readme.txt
+zip -9 -r "$2".zip "$1".pdf readme.txt
 mv "$2".zip encrypted_zip_files
 rm -r "$1".pdf
