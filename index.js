51c54a4ebd4783ff6522e77b698e17ff667fbd63
dudox <trybemark@gmail.com>
2020-06-12 11:32:34 +0100

    updates

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.post("/download", (req, res) => {
     res.header('Access-Control-Allow-Origin', req.headers.origin || "*");
     res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,HEAD,DELETE,OPTIONS');
     res.header('Access-Control-Allow-Headers', 'content-Type,x-requested-with');
-    exec("cd " + repo + "&& sh encrypt.sh "+ req.body.key, (err, stdout, stderr) => {
+    exec("cd " + repo + "&& sh encrypt.sh "+ req.body.key+ " "+req.body.name , (err, stdout, stderr) => {
         console.log(stdout);
         console.log(stderr);
         if (err) {
