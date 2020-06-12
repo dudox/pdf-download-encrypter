b5f7cfce6225cf719c07afddafbd225f6ebe1010
dudox <trybemark@gmail.com>
2020-06-12 03:17:58 +0100

    download scripts

--- encrypt.sh
+++ encrypt.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-DIR_PDF= "cd file && main.pdf"
-DIR_PROTECTED = "cd file && protected.pdf"
-pdftk DIR_PDF output DIR_PROTECTED  owner_pw 12345 user_pw
+
+pdftk  `$0` output DIR_PROTECTED  owner_pw 12345 user_pw
